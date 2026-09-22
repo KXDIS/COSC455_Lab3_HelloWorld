520202a85fa393bd816ba216546c7899e34049bc
Hendrick Nguyen <hendrick.nguyen079@gmail.com>
2026-09-22 11:33:46 -0400

    Task 5 finished

---
+++ target/debug/deps/hello_world-1bb7919254ac3d8a.d
@@ -0,0 +1,5 @@
+C:\Users\krono\hello_world\hello_world\target\debug\deps\hello_world-1bb7919254ac3d8a.d: src\main.rs
+
+C:\Users\krono\hello_world\hello_world\target\debug\deps\hello_world-1bb7919254ac3d8a.exe: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/hello_world-b97e9c536663361b.d
@@ -0,0 +1,5 @@
+C:\Users\krono\hello_world\hello_world\target\debug\deps\hello_world-b97e9c536663361b.d: src\lib.rs
+
+C:\Users\krono\hello_world\hello_world\target\debug\deps\hello_world-b97e9c536663361b.exe: src\lib.rs
+
+src\lib.rs:
